3fd2b4f082b42971c8ef4a324af9a65f1e6c765a
serena di santo <serenadisanto@ugr.es>
2023-03-12 18:36:55 +0000

    First commit

---
+++ Problema1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//////////////////////////////////////////
+// This program computes....
+// The inputs it uses are....
+// The outputs are....
+// remember to use a comment for every line of code
+// remember to give meaningful names to every variable
+// 
+/////////////////////////////////////////
+
+int main()
+{
+    printf("%lf",2000.);
+}
