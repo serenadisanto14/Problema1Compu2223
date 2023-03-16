4fc13aca33e0fddcfbba9b4ed5fe1b09e9a079be
serena di santo <serenadisanto@ugr.es>
2023-03-16 12:41:42 +0000

    para repositorio remoto

--- Problema1.cpp
+++ Problema1.cpp
@@ -3,15 +3,16 @@
 //////////////////////////////////////////
 // This program computes....
 // The inputs it uses are....
-// The outputs are....
+// fwse/lkdfhgro;z/ekigysfEV,khgvukzshb
 // remember to use a comment for every line of code
-// remember to give meaningful names to every variable
+// remember to give mingful names to every variable
 /////////////////////////////////////////
 
 int main()
 {
     printf("%lf",2000.); //adding a comment about this useless line
     //por ahora estoy satisfecha de mi programa! 
+    //esta linea /lkhjssoi?
     //quiero subirlo a github!!
 
 
