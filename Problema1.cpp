302063e7b07c37620f6bb96325ebeaaadd058600
serena di santo <serenadisanto@ugr.es>
2023-03-16 11:22:11 +0000

    showing a commit

--- Problema1.cpp
+++ Problema1.cpp
@@ -6,6 +6,7 @@
 // The outputs are....
 // remember to use a comment for every line of code
 // remember to give meaningful names to every variable
+// adding a new line here
 /////////////////////////////////////////
 
 int main()
@@ -13,4 +14,6 @@ int main()
     printf("%lf",2000.); //adding a comment about this useless line
     //por ahora estoy satisfecha de mi programa! 
     //quiero subirlo a github!!
+
+
 }
